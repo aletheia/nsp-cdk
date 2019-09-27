467fb3c157226b01c2734255ba0e3e54d656fd56
aletheia <bianchi.luca@gmail.com>
2019-09-27 13:21:14 +0200

    :recycle: Removed bootstrap app. refs #1
    Removed app from project because it's not needed for a library

--- src/app/index.ts
+++
@@ -1,6 +0,0 @@
-import cdk = require('@aws-cdk/core');
-import { MyConstruct } from '../lib/MyConstruct';
-
-const myExampleApp = new cdk.App();
-/* tslint:disable:no-unused-expression */
-new MyConstruct(myExampleApp, 'MyExampleApp');
