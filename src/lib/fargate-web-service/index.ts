e0dfea461f2b95fcbdcd9a800639d1227c0dc882
aletheia <bianchi.luca@gmail.com>
2019-09-27 13:17:02 +0200

    :sparkles: Fargate Web Construct. refs #1
    Add Fargate service construct to support web service creation

---
+++ src/lib/fargate-web-service/index.ts
@@ -0,0 +1,40 @@
+import { ContainerImage } from "@aws-cdk/aws-ecs";
+import { Construct } from "@aws-cdk/core";
+import {
+  Fargate,
+  FargateProps,
+  FargateServiceType,
+  ImageSourceType
+} from "../fargate";
+
+export interface ApplicationProps {
+  name?: string;
+  dockerImage: ContainerImage | string;
+}
+
+export class ApplicationEndpoint extends Construct {
+  constructor(scope: Construct, id: string, props: ApplicationProps) {
+    super(scope, id);
+    const { dockerImage } = props;
+    let { name } = props;
+    if (!name) {
+      name = `${id}Fargate`;
+    }
+    let sourceType: ImageSourceType;
+    if (typeof dockerImage === "string") {
+      if (dockerImage.startsWith("aws:")) {
+        sourceType = ImageSourceType.REPOSITORY_ARN;
+      } else if (dockerImage.startsWith("./")) {
+        sourceType = ImageSourceType.ASSET;
+      }
+    } else {
+    }
+    const fargateProps: FargateProps = {
+      name,
+      imageSource: dockerImage,
+      imageSourceType: sourceType!,
+      serviceType: FargateServiceType.LOAD_BALANCED
+    };
+    const fargate = new Fargate(this, id, fargateProps);
+  }
+}
